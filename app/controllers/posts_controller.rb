fe9f7e47a4cea16fac58c42699606b0c6c3d897d
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-03-13 10:19:36 +0100

    add controller crud, index and new controller post functions and add line in post views to change path in form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,49 @@
 class PostsController < ApplicationController
   def index
+    @posts = Posts.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+    redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(article_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
   
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
