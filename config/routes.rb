8bc9327cbb53a9e126c523fe12619e30bcc346a3
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-03-14 14:06:07 +0100

    add contact controller,views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-
+  resources :contact
   resources :posts
   resources :mainpages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
