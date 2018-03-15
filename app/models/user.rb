46bf35f2631e8ff66b0e458331752ef1e03ddc0c
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-03-15 11:36:01 +0100

    add role for user and generate migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: {guest: 0, commonuser:1, admin:2, superadmin:3 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
