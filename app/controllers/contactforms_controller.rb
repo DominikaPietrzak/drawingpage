7d6bb35296252ed905d6a34990dc21fabbf3ffc1
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-03-20 22:39:42 +0100

    create contactform model controller and views for making mails contact in the page

--- app/controllers/contactforms_controller.rb
+++ app/controllers/contactforms_controller.rb
@@ -1,2 +1,17 @@
 class ContactformsController < ApplicationController
+
+  def new
+    @contactform = Contactform.new
+  end
+
+  def create
+    @contactform = Contactform.new(params[:contactform])
+    @contactform.request = request
+    if @contactform.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = 'Cannot send message'
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
 
+  get 'contacts/index'
+
+  get 'contacts/new'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  resources :contactforms, only: [:create, :new, :index]
   resources :aboutmeinfos, only: [:index]
   resources :contacts, only: [:index]
   resources :posts do
   resources :comments
   end
   resources :mainpages, only:[:index]
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "mainpages#index"
 
